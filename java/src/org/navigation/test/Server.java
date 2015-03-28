e731231321955a89313adb94dd779b5413068f3c
Sifat <sifatsultan@gmail.com>
2015-03-28 08:49:36 +0400

    This is to test the Client

---
+++ java/src/org/navigation/test/Server.java
@@ -0,0 +1,38 @@
+package org.navigation.test;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Inet4Address;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+	
+	public Server(){
+		
+		try {
+			System.out.print("IP:"+ Inet4Address.getLocalHost());
+			ServerSocket server = new ServerSocket(8080);
+			Socket client = server.accept();
+			System.out.print("Client connected");
+			
+			DataInputStream is = new DataInputStream(client.getInputStream());
+			BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+			
+			while(true){
+				String msg = reader.readLine();
+				System.out.print(msg);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void main(String[] args){
+		Server myServer = new Server();
+	}
+
+}
